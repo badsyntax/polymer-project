7e3e2a306b1c4c749d300145dc56769eb7ef4504
Richard <willis.rh@gmail.com>
2014-06-28 02:47:27 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    nodewebkit: {
+      options: {
+        build_dir: './build',
+        mac: false,
+        win: false,
+        linux32: false,
+        linux64: true
+      },
+      src: ['./app**/*'] // Your node-webkit app
+    },  
+  });
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('release', ['nodewebkit']);
+  grunt.registerTask('default', ['release']);
+};
---
+++ README.md
@@ -0,0 +1,23 @@
+## Polymer Project
+
+A test project.
+
+### Dev setup:
+
+Install dependencies:
+
+`npm install && bower install`
+
+### Dev tools:
+
+Run the app in node-webkit:
+
+`npm run nodewebkit`
+
+Compile the Sass:
+
+`npm run build-css`
+
+Create a node-webkit packaged app:
+
+`npm run release`
